7d82e60120918222da23c50bba8351e60b2ef9fb
marcospaulorr <marcospauloricartereboucas@gmail.com>
2024-02-08 10:41:30 -0300

    parte 3 feita

--- servidor.py
+++ servidor.py
@@ -1,38 +1,11 @@
 import socket
 from pathlib import Path
-from utils import extract_route, read_file, load_data
+from utils import extract_route, read_file
+from views import index
 
 CUR_DIR = Path(__file__).parent
-SERVER_HOST = 'localhost'
+SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8081
-
-NOTE_TEMPLATE = '''  <li>
-    <h3>{title}</h3>
-    <p>{details}</p>
-  </li>
-'''
-
-RESPONSE_TEMPLATE = '''HTTP/1.1 200 OK
-
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Get-it</title>
-</head>
-<body>
-
-<img src="img/logo-getit.png">
-<p>Como o Post-it, mas com outro verbo</p>
-
-<ul>
-{notes}
-</ul>
-
-</body>
-</html>
-'''
-
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -49,20 +22,16 @@ while True:
     print(request)
 
     route = extract_route(request)
+
     filepath = CUR_DIR / route
     if filepath.is_file():
-        response = 'HTTP/1.1 200 OK\n\n'.encode() + read_file(filepath)
+        response = read_file(filepath)
+    elif route == '':
+        response = index()
     else:
-        # Cria uma lista de <li>'s para cada anotação
-        # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
-        notes_li = [
-            NOTE_TEMPLATE.format(title=dados['titulo'], details=dados['detalhes'])
-            for dados in load_data('notes.json')
-        ]
-        notes = '\n'.join(notes_li)
+        response = bytes()
 
-        response = RESPONSE_TEMPLATE.format(notes=notes).encode()
-    client_connection.sendall(response)
+    client_connection.sendall('HTTP/1.1 200 OK\n\n'.encode() + response)
 
     client_connection.close()
 
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 def read_file(filepath):
     with open(filepath, 'rb') as file:
         return file.read()
@@ -5,3 +7,34 @@ def read_file(filepath):
 def extract_route(request):
     return request.split()[1].lstrip('/')
 
+def load_data(filename):
+    # Constrói o caminho do arquivo assumindo que ele está na pasta 'data'
+    filepath = f"data/{filename}"
+
+    # Abre o arquivo e carrega o conteúdo
+    try:
+        with open(filepath, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Arquivo {filename} não encontrado.")
+    except json.JSONDecodeError:
+        print(f"Erro ao decodificar o arquivo {filename}. Verifique se é um JSON válido.")
+    except Exception as e:
+        print(f"Ocorreu um erro ao carregar o arquivo {filename}: {e}")
+
+def load_template(filename):
+    # Constrói o caminho do arquivo assumindo que ele está na pasta 'templates'
+    filepath = f"templates/{filename}"
+
+    try:
+        # Abre o arquivo para leitura
+        with open(filepath, 'r', encoding='utf-8') as file:
+            # Lê o conteúdo do arquivo e o retorna
+            return file.read()
+    except FileNotFoundError:
+        print(f"Arquivo {filepath} não encontrado.")
+        return None
+    except Exception as e:
+        print(f"Ocorreu um erro ao ler o arquivo {filepath}: {e}")
+        return None
