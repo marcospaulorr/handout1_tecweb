26bece38314c06d2c6cf6ca52452b19559618bb2
marcospaulorr <marcospauloricartereboucas@gmail.com>
2024-02-08 09:26:02 -0300

    v

--- servidor.py
+++ servidor.py
@@ -1,5 +1,8 @@
 import socket
+from pathlib import Path
+from utils import extract_route, read_file
 
+CUR_DIR = Path(__file__).parent
 SERVER_HOST = 'localhost'
 SERVER_PORT = 8081
 
@@ -14,7 +17,39 @@ RESPONSE_TEMPLATE = '''HTTP/1.1 200 OK
 <body>
 
 <h1>Get-it</h1>
+<img src="img/logo-getit(1).png">
 <p>Como o Post-it, mas com outro verbo</p>
+
+<ul>
+  <li>
+    <h3>Receita de miojo</h3>
+    <p>Bata com um martelo antes de abrir o pacote. Misture o tempero, coloque em uma vasilha e aproveite seu snack :)</p>
+  </li>
+  <li>
+    <h3>Pão doce</h3>
+    <p>Abra o pão e coloque o seu suco em pó favorito.</p>
+  </li>
+  <li>
+    <h3>Sorvete com cristais de leite</h3>
+    <p>Sirva o seu sorvete favorito em uma vasilha e jogue leite em cima.</p>
+  </li>
+  <li>
+    <h3>Iogurte natural</h3>
+    <p>Deixe o leite fora da geladeira (esse é mentira, não faça isso).</p>
+  </li>
+  <li>
+    <h3>Homer Simpson</h3>
+    <p>~( 8(|)</p>
+  </li>
+  <li>
+    <h3>Numero mágico</h3>
+    <p>142857</p>
+  </li>
+  <li>
+    <h3>Série da Fundação - Isaac Asimov</h3>
+    <p>É boa, leia.</p>
+  </li>
+</ul>
 
 </body>
 </html>
@@ -35,7 +70,13 @@ while True:
     print('*'*100)
     print(request)
 
-    client_connection.sendall(RESPONSE_TEMPLATE.encode())
+    route = extract_route(request)
+    filepath = CUR_DIR / route
+    if filepath.is_file():
+        response = 'HTTP/1.1 200 OK\n\n'.encode() + read_file(filepath)
+    else:
+        response = RESPONSE_TEMPLATE.encode()
+    client_connection.sendall(response)
 
     client_connection.close()
 
---
+++ utils.py
@@ -0,0 +1,7 @@
+def read_file(filepath):
+    with open(filepath, 'rb') as file:
+        return file.read()
+
+def extract_route(request):
+    return request.split()[1].lrstrip('/')
+
